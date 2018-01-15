33701c55566b088b582c22dbaf7f2861f908b1e9
mrmakan <makan85dostdar@gmail.com>
2018-01-15 09:40:54 +0330

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -57,7 +57,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by @LuaError                \e[00;37;40m"
+echo -e "               \e[01;34m                    Makan Dustdar                \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -208,7 +208,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by @LuaError                \e[00;37;40m"
+echo -e "               \e[01;34m                    Makan Dustdar                \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
